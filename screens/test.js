9c0c2d6b56db893e635d5def9e9be5dfb1532c43
Niyaz Niyazov <75684807+ncniyazov@users.noreply.github.com>
2025-06-06 23:24:42 +0400

    Update test.js

--- screens/test.js
+++ screens/test.js
@@ -1,4 +1,7 @@
-<div style="width:100%;height:100px;background:#2196F3;color:white;
-    text-align:center;padding:20px;font-size:20px;border-radius:10px;">
-  Demo JS Reklam 
+<div onclick="window.open('https://niyazed.pro', '_blank')" 
+     style="width:100%;height:100px;background:#2196F3;color:white;
+     text-align:center;padding:20px;font-size:20px;border-radius:10px;
+     cursor:pointer;display:flex;align-items:center;justify-content:center;
+     font-family:sans-serif;transition:0.3s;">
+  🔥 Reklama kliklə!
 </div>
